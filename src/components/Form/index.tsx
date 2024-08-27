61fd24ace9d99e290a174122a0dbc183d3a473e5
kauan02 <kauanbrezende82@gmail.com>
2024-08-27 13:05:08 -0300

    Commit 3

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '../Button';
+import './style.scss'
 
 class Form extends React.Component{
     render() {
         return(
-            <form>
-                <div>
+            <form className="newTask">
+                <div className="inputContainer">
                     <label htmlFor="task">
                         Add a new task
                     </label>
@@ -17,7 +18,7 @@ class Form extends React.Component{
                         required
                     />
                 </div>
-                <div>
+                <div className="inputContainer">
                     <label htmlFor='time'>
                         Time
                     </label>
